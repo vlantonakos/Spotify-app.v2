fbe74e2bd1cbac2e233f31a4bcc52ea1ac5622c0
Antonis <107072477+vlantonakos@users.noreply.github.com>
2023-05-21 12:09:44 +0300

    Add files via upload

---
+++ spotify-app/components/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Player = () => {
+  return (
+    <div className='h-24 bg-neutral-800 border-t border-neutral-700 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
+      Player
+    </div>
+  )
+}
+
+export default Player
---
+++ spotify-app/components/Song.jsx
@@ -0,0 +1,11 @@
+import { PlayIcon } from '@heroicons/react/24/solid';
+import { useSession } from 'next-auth/react';
+import React, { useState } from 'react';
+
+const Song = ({ sno, track, setGlobalCurrentSongId, setGlobalIsTrackPlaying, setView, setGlobalArtistId }) => {
+  return (
+    <div>Song</div>
+  )
+}
+
+export default Song
